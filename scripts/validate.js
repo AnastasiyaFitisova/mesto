963fb9144c66f4b11cc0705003473f7c635611f7
Anastasiya Fitisova <Anastasiya.Fitisova@yandex.ru>
2022-04-28 19:07:55 +0300

    feat: добавить валидацию форм

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,61 +1,56 @@
-function enableValidation(formObj) {
-  const form = document.querySelector(formObj.formSelector);
+//Элементы формы
+const formElement = document.querySelector('.popup__form'); //форма
+const formInput = formElement.querySelector('.popup__input'); //input
 
-  const inputs = document.querySelectorAll(formObj.inputSelector);
-  inputs.forEach((element) => {
-    element.addEventListener('input', (evt) => handleInputAdd(evt, form, formObj))
-  })
-
-  const formList = Array.from(document.querySelectorAll(formObj.formSelector));
-  formList.forEach((element) => {
-    element.addEventListener('submit', (evt) => handleFormSubmit(evt, form, formObj))
-    handleInputAdd(element);
-  });
-
-  form.addEventListener('submit', (evt) => handleFormSubmit(evt, form));
-
-  toggleButton(form, formObj);
+//Функции стилей и сообщения ошибки
+const showInputError = (formElement, formInput, errorMessage) => {
+  const formError = formElement.querySelector(`#${formInput.id}-error`);
+  formInput.classList.add('popup__input_not-valid');
+  formError.textContent = errorMessage;
 }
 
-//функция работы с состоянием кнопки
-function toggleButton(form, formObj) {
-  const button = document.querySelector(formObj.buttonSelector);
-  button.disabled = !form.checkValidity();
-  button.classList.toggle('popup__submit-button_disabled', !form.checkValidity());
+const hideInputError = (formElement, formInput) => {
+  const formError = formElement.querySelector(`#${formInput.id}-error`);
+  formInput.classList.remove('popup__input_not-valid');
+  formError.textContent = '';
 }
 
-//функции изменения стиля инпута
-const showInputError = (element) => {
-  element.classList.add('popup__input_not-valid');
-  
-};
-const hideInputError = (element) => {
-  element.classList.remove('popup__input_not-valid');
-};
-
-//сабмит формы
-function handleFormSubmit(evt, form) {
-  evt.preventDefault();
-}
-
-//вызов или удаление стилей и сообщений об ошибках
-function handleInputAdd(evt, form, formObj) {
-  const input = evt.target;
-  const error = document.querySelector(`#${input.id}-error`);
-  
-  if (input.validity.valid) {
-    error.textContent = '';
-    hideInputError(input)
+const checkInputValidity = (formElement, formInput) => {
+  if (!formInput.validity.valid) {
+    showInputError(formElement, formInput, formInput.validationMessage);
   } else {
-    error.textContent = input.validationMessage;
-    showInputError(input)
+    hideInputError(formElement, formInput);
   }
+};
 
-  toggleButton(form, formObj);
-}
-
-enableValidation ({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  buttonSelector: '.popup__submit-button',
+formElement.addEventListener('submit', function (evt) {
+  evt.preventDefault();
 });
+
+formInput.addEventListener('input', function () {
+  checkInputValidity();
+});
+
+const setEventListeners = (formElement) => {
+  const inputs = Array.from(formElement.querySelectorAll('.form__input'));
+  
+  inputs.forEach((formInput) => {
+    formInput.addEventListener('input', () => {
+    checkInputValidity(formElement, formInput)
+  });
+});
+
+};
+
+const enableValidation = () => {
+  const forms = Array.from(document.querySelectorAll('.form'));
+
+  forms.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation(); 
